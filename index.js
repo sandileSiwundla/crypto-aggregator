e01630a67bde9294234c274f6e4c4411e74034bd
sandileSiwundla <sandsmthembu2@gmail.com>
2025-11-05 00:25:24 +0200

    Working om vercel

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const allRoutes = require('./routes/all');
 const singleRoutes = require('./routes/single');
 
+// API routes
 app.use('/api/all', allRoutes);
 app.use('/api/single', singleRoutes);
 
-
-// Serve static files (CSS, JS)
+// Serve static files (CSS, JS) - important for Vercel
 app.use(express.static('public'));
 
-// Landing page
+// HTML pages
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// All cryptos page
 app.get('/all', (req, res) => {
   res.sendFile(__dirname + '/views/all-cryptos.html');
 });
 
-// Single token page
 app.get('/token', (req, res) => {
   res.sendFile(__dirname + '/views/single-token.html');
 });
 
-// Compare tokens page
 app.get('/compare', (req, res) => {
   res.sendFile(__dirname + '/views/compare.html');
 });
 
-app.listen(PORT, () => console.log(`Server running: http://localhost:${PORT}`));
+// Export for Vercel
+module.exports = app;
+
+// Only run server locally
+if (require.main === module) {
+  app.listen(PORT, () => console.log(`Server running: http://localhost:${PORT}`));
+}
