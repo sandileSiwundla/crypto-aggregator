91512d84681d896de4064a714c0019e9dda483c3
sandileSiwundla <sandsmthembu2@gmail.com>
2026-01-05 01:07:00 +0200

    Moving to standard for vercel

--- api/app.js
+++ api/app.js
@@ -7,8 +7,7 @@ const PORT = 3000;
 // ✅ Serve everything from /public
 app.use(express.static(path.join(__dirname, '../public')));
 
-// ❌ DO NOT manually serve index.html anymore
-// ❌ DO NOT use app.get('*') for frontend
+
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
