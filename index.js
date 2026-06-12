93bd82fd73381e2b7b5b5e150c04b1cecf5d6d27
26206upskill <sandsmthembu2@gmail.com>
2026-06-12 15:25:44 +0200

    weee

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT =  3001;
 
 const singleRoutes = require('./routes/single');
 
